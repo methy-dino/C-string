693887d2aaa0637aaf5a59627ed97240830898e0
methy-dino <gutotlima@gmail.com>
2025-02-04 11:09:34 -0300

    Added append methods for most common str-like cases.

--- string.c
+++ string.c
@@ -14,9 +14,51 @@ struct string* initString(char* pointer, int length){
 	}
 	return string;
 }
-struct string* appendArr(struct string* string, char[]chars, int arrL){
+struct string* appendArr(struct string* str, char[]chars, int arrL){
 	for (int i = 0; i < arrL; i++){
-		if (string->length == string->maxCapacity){
-			growStr(string, (string->length+1) / 2)   
+		if (str->length == str->maxCapacity){
+			growStr(str, (str->length+1) / 2);  
 		}
+		str->string[str->length] = chars[i];
+		str->length++;
+	}
 }
+struct string* appendPtr(struct string* str, char* ptr, int ptrLen){
+	for (int i = 0; i < ptrLen; i++){
+		if (str->length == str->maxCapacity){
+			growStr(str, (str->length+1) / 2);  
+		}
+		str->string[str->length] = ptr[i];
+		str->length++;
+	}
+	return str;
+}
+struct string* appendHeapPtr(struct string* str, char* ptr, int ptrLen){
+	for (int i = 0; i < ptrLen; i++){
+		if (str->length == str->maxCapacity){
+			growStr(str, (str->length+1) / 2);   
+		}
+		str->string[str->length] = ptr[i];
+		str->length++;
+	}
+	free(ptr);
+	return str;
+}
+struct string* appendStr(struct string* str, struct string* toAppend){
+	for (int i = 0; i < toAppend.length; i++){
+		if (str->length == str->maxCapacity){
+			growStr(str, (str->length+1) / 2);  
+		}
+		str->string[str->length] = toAppend->string[i];
+		str->length++;
+	}
+	return str;	
+}	
+void growStr(struct string* str, int inc){
+	int newL = inc + str->maxCapacity;
+	char* nStr = malloc(newL);
+	for (int i = 0; i < str->len; i++){
+		nStr[i] = str->string[i];
+	}
+}
+
