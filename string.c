b9399f17cb665ed75b668bd142632c9ecc418c30
methy-dino <gutotlima@gmail.com>
2025-02-05 13:56:50 -0300

    new constructors, fixed memory leak in growStr(String*, int);

--- string.c
+++ string.c
@@ -10,14 +10,34 @@ void growStr(String* str, int inc){
 	for (int i = 0; i < str->length; i++){
 		nStr[i] = str->string[i];
 	}
+	free(str->string);
+	str->string = nStr;
 	str->string[str->length] = '\0';
+}
+String* emptyStr(int allocSize){
+	String* string  = (String*)malloc(sizeof(struct string));
+	string->maxCapacity = allocSize;
+	string->length = 0;
+	string->string = (char*)malloc(string->maxCapacity);
+	return string;
+}
+String* charArrToStr(char arr[], int length){
+	String* string  = (String*)malloc(sizeof(struct string));
+	string->maxCapacity = length*1.5+1;
+	string->length = length;
+	string->string = (char*)malloc(string->maxCapacity);
+	for (int i = 0; i < length; i++){
+		string->string[i] = arr[i];
+	}
+	string->string[string->length] = '\0';
+	return string;
 }
 String* initString(char* pointer, int length){
 		String* string  = (String*)malloc(sizeof(struct string));
 		string->maxCapacity = length*1.5+1;
 		string->length = length;
 		string->string = (char*)malloc(string->maxCapacity);
-	for (int i = 0; i < sizeof(pointer); i++){
+	for (int i = 0; i < length; i++){
 		string->string[i] = pointer[i];
 	}
 	string->string[string->length] = '\0';
@@ -71,7 +91,7 @@ String* appendStr(String* str, String* toAppend){
 	str->string[str->length] = '\0';
 	return str;	
 }
-String* appendHeapStr(String* str, String* toAppend){
+String* concatStr(String* str, String* toAppend){
 	if (str->maxCapacity < str->length + toAppend->length){
 	 growStr(str, toAppend->length * 1.5);
 	}
@@ -117,4 +137,57 @@ void removeSubStr(String* str, String* subStr){
 	}
 	str->length-=removed;
 	str->string[str->length] = '\0';
+}
+int indexOfChar(String* str, char character, int startIndex){
+	int start = (str->length + startIndex) % str->length;
+	while (start < str->length){
+		if (str->string[start] == character){
+			return start;
+		}
+		start++;
+	}
+	return -1;
+}
+int lastIndexOfChar(String* str, char character, int endOffset){
+	int start = (str->length + endOffset - 1) % str->length;
+	while (start > -1){
+		if (str->string[start] == character){
+			return start;
+		}
+		start--;
+	}
+	return -1;
+}
+int indexOfStr(String* str, String* subStr, int startIndex){
+	int start = (str->length + startIndex) % str->length;
+	int i = 0;
+	while (start < str->length){
+/* must be done in this way, for cases like ("abaabaac", "abaac")
+* since the string start can be messed up by an check advancing after it
+*  It's possible to check if the start is seen, but it has barely any benefit*/
+		while (str->string[start+i] == subStr->string[i]){
+			i++;
+			if (i+1 == subStr->length){
+				return start;
+			}
+		}
+		i = 0;
+		start++;
+	}
+	return -1;
+}
+int LastindexOfStr(String* str, String* subStr, int endOffset){
+	int start = (str->length + endOffset - 1) % str->length;
+	int i = subStr->length;
+	while (start > 0){
+		while (str->string[start-i] == subStr->string[subStr->length - i]){
+			i--;
+			if (i == 1){
+				return start;
+			}	
+		}
+		start--;
+		i = subStr->length;
+	}
+	return -1;
 }
